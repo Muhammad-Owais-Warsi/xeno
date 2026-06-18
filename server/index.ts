a8ac574953cd69ca3b6ac3effb52630a9fde5382
Muhammad-Owais-Warsi <warsimuhammadowais@gmail.com>
2026-06-18 17:54:14 +0530

    updae backend url

--- server/index.ts
+++ server/index.ts
@@ -1,22 +1,26 @@
-import express from 'express'
-import cors from 'cors'
-import path from 'path'
-import uploadRoutes from './src/routes/uploadRoutes'
-import { ensureDir } from './src/utils/fileUtils'
+import express from "express";
+import cors from "cors";
+import path from "path";
+import uploadRoutes from "./src/routes/uploadRoutes";
+import { ensureDir } from "./src/utils/fileUtils";
 
-const app = express()
-const PORT = process.env.PORT || 3001
+const app = express();
+const PORT = process.env.PORT || 3001;
 
-const OUTPUT_DIR = path.resolve('output')
-ensureDir(OUTPUT_DIR)
+const OUTPUT_DIR = path.resolve("output");
+ensureDir(OUTPUT_DIR);
 
-app.use(cors())
-app.use(express.json())
+app.use(
+  cors({
+    origin: "https://xeno-client.onrender.com",
+  }),
+);
+app.use(express.json());
 
-app.use('/output', express.static(OUTPUT_DIR))
+app.use("/output", express.static(OUTPUT_DIR));
 
-app.use('/api', uploadRoutes)
+app.use("/api", uploadRoutes);
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`)
-})
+  console.log(`Server running on http://localhost:${PORT}`);
+});
